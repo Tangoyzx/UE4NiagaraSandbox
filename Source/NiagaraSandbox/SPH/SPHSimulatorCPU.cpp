0f33e61a49b5acb0b22a1a7a829c3275b87f764e
monguri <mongry@gmail.com>
2021-02-13 16:31:51 +0900

    SPHのCPUシミュレーションをしてNiagaraSystemに反映させるためのアクタを、まずは、NiagaraActorのソースコードを参考に、ただNiagaraComponentを保持するアクタとして新規追加。

---
+++ Source/NiagaraSandbox/SPH/SPHSimulatorCPU.cpp
@@ -0,0 +1,110 @@
+#include "SPHSimulatorCPU.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Engine/Texture2D.h"
+#include "Components/ArrowComponent.h"
+#include "Components/BillboardComponent.h"
+#include "NiagaraComponent.h"
+#include "NiagaraSystem.h"
+
+ASPHSimulatorCPU::ASPHSimulatorCPU()
+{
+	NiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("NiagaraComponent0"));
+
+	RootComponent = NiagaraComponent;
+
+#if WITH_EDITORONLY_DATA
+	SpriteComponent = CreateEditorOnlyDefaultSubobject<UBillboardComponent>(TEXT("Sprite"));
+	ArrowComponent = CreateEditorOnlyDefaultSubobject<UArrowComponent>(TEXT("ArrowComponent0"));
+
+	if (!IsRunningCommandlet())
+	{
+		// Structure to hold one-time initialization
+		struct FConstructorStatics
+		{
+			ConstructorHelpers::FObjectFinderOptional<UTexture2D> SpriteTextureObject;
+			FName ID_Effects;
+			FText NAME_Effects;
+			FConstructorStatics()
+				: SpriteTextureObject(TEXT("/Niagara/Icons/S_ParticleSystem"))
+				, ID_Effects(TEXT("Effects"))
+				, NAME_Effects(NSLOCTEXT("SpriteCategory", "Effects", "Effects"))
+			{
+			}
+		};
+		static FConstructorStatics ConstructorStatics;
+
+		if (SpriteComponent)
+		{
+			SpriteComponent->Sprite = ConstructorStatics.SpriteTextureObject.Get();
+			SpriteComponent->SetRelativeScale3D_Direct(FVector(0.5f, 0.5f, 0.5f));
+			SpriteComponent->bHiddenInGame = true;
+			SpriteComponent->bIsScreenSizeScaled = true;
+			SpriteComponent->SpriteInfo.Category = ConstructorStatics.ID_Effects;
+			SpriteComponent->SpriteInfo.DisplayName = ConstructorStatics.NAME_Effects;
+			SpriteComponent->SetupAttachment(NiagaraComponent);
+			SpriteComponent->bReceivesDecals = false;
+		}
+
+		if (ArrowComponent)
+		{
+			ArrowComponent->ArrowColor = FColor(0, 255, 128);
+
+			ArrowComponent->ArrowSize = 1.5f;
+			ArrowComponent->bTreatAsASprite = true;
+			ArrowComponent->bIsScreenSizeScaled = true;
+			ArrowComponent->SpriteInfo.Category = ConstructorStatics.ID_Effects;
+			ArrowComponent->SpriteInfo.DisplayName = ConstructorStatics.NAME_Effects;
+			ArrowComponent->SetupAttachment(NiagaraComponent);
+			ArrowComponent->SetUsingAbsoluteScale(true);
+		}
+	}
+#endif // WITH_EDITORONLY_DATA
+}
+
+void ASPHSimulatorCPU::PostRegisterAllComponents()
+{
+	Super::PostRegisterAllComponents();
+
+	// Set Notification Delegate
+	if (NiagaraComponent)
+	{
+		NiagaraComponent->OnSystemFinished.AddUniqueDynamic(this, &ASPHSimulatorCPU::OnNiagaraSystemFinished);
+	}
+}
+
+void ASPHSimulatorCPU::SetDestroyOnSystemFinish(bool bShouldDestroyOnSystemFinish)
+{
+	bDestroyOnSystemFinish = bShouldDestroyOnSystemFinish ? 1 : 0;  
+};
+
+void ASPHSimulatorCPU::OnNiagaraSystemFinished(UNiagaraComponent* FinishedComponent)
+{
+	if (bDestroyOnSystemFinish)
+	{
+		SetLifeSpan(0.0001f);
+	}
+}
+
+#if WITH_EDITOR
+bool ASPHSimulatorCPU::GetReferencedContentObjects(TArray<UObject*>& Objects) const
+{
+	Super::GetReferencedContentObjects(Objects);
+
+	if (UNiagaraSystem* System = NiagaraComponent->GetAsset())
+	{
+		Objects.Add(System);
+	}
+
+	return true;
+}
+
+void ASPHSimulatorCPU::ResetInLevel()
+{
+	if (NiagaraComponent)
+	{
+		NiagaraComponent->Activate(true);
+		NiagaraComponent->ReregisterComponent();
+	}
+}
+#endif // WITH_EDITOR
+
---
+++ Source/NiagaraSandbox/SPH/SPHSimulatorCPU.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "GameFramework/Actor.h"
+#include "SPHSimulatorCPU.generated.h"
+
+UCLASS()
+class ASPHSimulatorCPU : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASPHSimulatorCPU();
+
+	virtual void PostRegisterAllComponents() override;
+
+	/** Set true for this actor to self-destruct when the Niagara system finishes, false otherwise */
+	UFUNCTION(BlueprintCallable, Category=NiagaraActor)
+	void SetDestroyOnSystemFinish(bool bShouldDestroyOnSystemFinish);
+
+private:
+	/** Pointer to System component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=NiagaraActor, meta = (AllowPrivateAccess = "true"))
+	class UNiagaraComponent* NiagaraComponent;
+
+#if WITH_EDITORONLY_DATA
+	// Reference to sprite visualization component
+	UPROPERTY()
+	class UBillboardComponent* SpriteComponent;
+
+	// Reference to arrow visualization component
+	UPROPERTY()
+	class UArrowComponent* ArrowComponent;
+
+#endif
+
+	/** True for this actor to self-destruct when the Niagara system finishes, false otherwise */
+	UPROPERTY()
+	uint32 bDestroyOnSystemFinish : 1;
+
+	/** Callback when Niagara system finishes. */
+	UFUNCTION(CallInEditor)
+	void OnNiagaraSystemFinished(UNiagaraComponent* FinishedComponent);
+
+public:
+	/** Returns NiagaraComponent subobject **/
+	class UNiagaraComponent* GetNiagaraComponent() const { return NiagaraComponent; }
+#if WITH_EDITORONLY_DATA
+	/** Returns SpriteComponent subobject **/
+	class UBillboardComponent* GetSpriteComponent() const { return SpriteComponent; }
+	/** Returns ArrowComponent subobject **/
+	class UArrowComponent* GetArrowComponent() const { return ArrowComponent; }
+#endif
+
+#if WITH_EDITOR
+	// AActor interface
+	virtual bool GetReferencedContentObjects(TArray<UObject*>& Objects) const override;
+	// End of AActor interface
+
+	/** Reset this actor in the level.*/
+	void ResetInLevel();
+#endif // WITH_EDITOR
+};
+
