34d8996c75703e4cc6a5e3063061ac8c854f77ee
monguri <mongry@gmail.com>
2021-03-23 22:18:00 +0900

    SPH2DSimulatorCPUをコピペしてクラス名だけ変えたSPH3DSimulatorCPUを追加

---
+++ Source/NiagaraSandbox/SPH/SPH3DSimulatorCPU.cpp
@@ -0,0 +1,489 @@
+#include "SPH3DSimulatorCPU.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Engine/Texture2D.h"
+#include "Components/ArrowComponent.h"
+#include "Components/BillboardComponent.h"
+#include "NiagaraComponent.h"
+#include "NiagaraSystem.h"
+#include "NiagaraFunctionLibrary.h"
+#include "NiagaraDataInterfaceArrayFloat.h"
+
+namespace
+{
+	// UNiagaraDataInterfaceArrayFunctionLibrary::SetNiagaraArrayVector()を参考にしている
+	void SetNiagaraArrayVector(UNiagaraComponent* NiagaraSystem, FName OverrideName, const TArray<FVector>& ArrayData)
+	{
+		if (UNiagaraDataInterfaceArrayFloat3* ArrayDI = UNiagaraFunctionLibrary::GetDataInterface<UNiagaraDataInterfaceArrayFloat3>(NiagaraSystem, OverrideName))
+		{
+			FRWScopeLock WriteLock(ArrayDI->ArrayRWGuard, SLT_Write);
+			ArrayDI->FloatData = ArrayData;
+			ArrayDI->MarkRenderDataDirty();
+		}
+	}
+}
+
+void ASPH3DSimulatorCPU::BeginPlay()
+{
+	Super::BeginPlay();
+
+	Positions.SetNum(NumParticles);
+	Velocities.SetNum(NumParticles);
+	Accelerations.SetNum(NumParticles);
+	Densities.SetNum(NumParticles);
+	Pressures.SetNum(NumParticles);
+	Positions3D.SetNum(NumParticles);
+
+	// InitPosRadius半径の円内にランダムに配置
+	for (int32 i = 0; i < NumParticles; ++i)
+	{
+		Positions[i] = WallBox.GetCenter() + FMath::RandPointInCircle(InitPosRadius);
+	}
+
+	for (int32 i = 0; i < NumParticles; ++i)
+	{
+		Velocities[i] = FVector2D::ZeroVector;
+	}
+
+	for (int32 i = 0; i < NumParticles; ++i)
+	{
+		Positions3D[i] = FVector(0.0f, Positions[i].X, Positions[i].Y);
+	}
+
+	if (bUseNeighborGrid3D)
+	{
+		//TODO: [-WorldBBoxSize / 2, WorldBBoxSize / 2]を[0,1]に写像して扱う。RotationとTranslationはなし
+		SimulationToUnitTransform = FTransform(FQuat::Identity, FVector(0.5f), FVector(1.0f) / FVector(1.0f, WorldBBoxSize.X, WorldBBoxSize.Y));
+		NeighborGrid3D.Initialize(FIntVector(1, NumCellsX, NumCellsY), MaxNeighborsPerCell);
+	}
+
+	// Tick()で設定しても、レベルにNiagaraSystemが最初から配置されていると、初回のスポーンでは配列は初期値を使ってしまい
+	//間に合わないのでBeginPlay()でも設定する
+	NiagaraComponent->SetNiagaraVariableInt("NumParticles", NumParticles);
+	SetNiagaraArrayVector(NiagaraComponent, FName("Positions"), Positions3D);
+
+	DensityCoef = Mass * 4.0f / PI / FMath::Pow(SmoothLength, 8);
+	GradientPressureCoef = Mass * -30.0f / PI / FMath::Pow(SmoothLength, 5);
+	LaplacianViscosityCoef = Mass * 20.0f / 3.0f / PI / FMath::Pow(SmoothLength, 5);
+
+	SmoothLenSq = SmoothLength * SmoothLength;
+	NumThreadParticles = (NumParticles + NumThreads - 1) / NumThreads;
+}
+
+void ASPH3DSimulatorCPU::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	if (DeltaSeconds > KINDA_SMALL_NUMBER)
+	{
+		// DeltaSecondsの値の変動に関わらず、シミュレーションに使うサブステップタイムは固定とする
+		float SubStepDeltaSeconds = 1.0f / FrameRate / NumIterations;
+
+		for (int32 i = 0; i < NumIterations; ++i)
+		{
+			Simulate(SubStepDeltaSeconds);
+		}
+	}
+
+	for (int32 i = 0; i < NumParticles; ++i)
+	{
+		Positions3D[i] = FVector(0.0f, Positions[i].X, Positions[i].Y);
+	}
+
+	NiagaraComponent->SetNiagaraVariableInt("NumParticles", NumParticles);
+	SetNiagaraArrayVector(NiagaraComponent, FName("Positions"), Positions3D);
+}
+
+void ASPH3DSimulatorCPU::Simulate(float DeltaSeconds)
+{
+	for (int32 ParticleIdx = 0; ParticleIdx < NumParticles; ++ParticleIdx)
+	{
+		Densities[ParticleIdx] = 0.0f;
+		Accelerations[ParticleIdx] = FVector2D::ZeroVector;
+	}
+
+	if (bUseNeighborGrid3D)
+	{
+		NeighborGrid3D.Reset();
+
+		// NeighborGrid3Dの構築
+		ParallelFor(NumThreads,
+			[this](int32 ThreadIndex)
+			{
+				for (int32 ParticleIdx = NumThreadParticles * ThreadIndex; ParticleIdx < NumThreadParticles * (ThreadIndex + 1) && ParticleIdx < NumParticles; ++ParticleIdx)
+				{
+					const FVector& UnitPos = NeighborGrid3D.SimulationToUnit(Positions3D[ParticleIdx], SimulationToUnitTransform);
+					const FIntVector& CellIndex = NeighborGrid3D.UnitToIndex(UnitPos);
+					if (NeighborGrid3D.IsValidCellIndex(CellIndex))
+					{
+						int32 LinearIndex = NeighborGrid3D.IndexToLinear(CellIndex);
+						int32 PreviousNeighborCount = 0;
+						NeighborGrid3D.SetParticleNeighborCount(LinearIndex, 1, PreviousNeighborCount);
+
+						if (PreviousNeighborCount < MaxNeighborsPerCell)
+						{
+							int32 NeighborGridLinearIndex = NeighborGrid3D.NeighborGridIndexToLinear(CellIndex, PreviousNeighborCount);
+							NeighborGrid3D.SetParticleNeighbor(NeighborGridLinearIndex, ParticleIdx);
+						}
+						else
+						{
+							UE_LOG(LogTemp, Warning, TEXT("Over registation to NeighborGrid3DCPU. CellIndex=(%d, %d, %d). PreviousNeighborCount=%d."), CellIndex.X, CellIndex.Y, CellIndex.Z, PreviousNeighborCount);
+						}
+					}
+					else
+					{
+						UE_LOG(LogTemp, Warning, TEXT("There is a particle which is out of NeighborGrid3D. Idx = %d. Position = (%f, %f, %f)."), ParticleIdx, Positions3D[ParticleIdx].X, Positions3D[ParticleIdx].Y, Positions3D[ParticleIdx].Z);
+						continue;
+					}
+				}
+			}
+		);
+
+		ParallelFor(NumThreads,
+			[this](int32 ThreadIndex)
+			{
+				for (int32 ParticleIdx = NumThreadParticles * ThreadIndex; ParticleIdx < NumThreadParticles * (ThreadIndex + 1) && ParticleIdx < NumParticles; ++ParticleIdx)
+				{
+					// キャッシュするほどのものでもないのでNeighborGrid3D構築のときと同じ計算をしているのは許容する
+					const FVector& UnitPos = NeighborGrid3D.SimulationToUnit(Positions3D[ParticleIdx], SimulationToUnitTransform);
+					const FIntVector& CellIndex = NeighborGrid3D.UnitToIndex(UnitPos);
+
+					if (!NeighborGrid3D.IsValidCellIndex(CellIndex))
+					{
+						// 構築のときに警告ログを出しているので警告を出すことはしない
+						continue;
+					}
+
+					static FIntVector AdjacentIndexOffsets[9] = {
+						FIntVector(0, -1, -1),
+						FIntVector(0, 0, -1),
+						FIntVector(0, +1, -1),
+						FIntVector(0, -1, 0),
+						FIntVector(0, 0, 0),
+						FIntVector(0, +1, 0),
+						FIntVector(0, -1, +1),
+						FIntVector(0, 0, +1),
+						FIntVector(0, +1, +1)
+					};
+
+					for (int32 AdjIdx = 0; AdjIdx < 9; ++AdjIdx)
+					{
+						const FIntVector& AdjacentCellIndex = CellIndex + AdjacentIndexOffsets[AdjIdx];
+						if (!NeighborGrid3D.IsValidCellIndex(AdjacentCellIndex))
+						{
+							continue;
+						}
+
+						for (int32 NeighborIdx = 0; NeighborIdx < MaxNeighborsPerCell; ++NeighborIdx)
+						{
+							int32 NeighborLinearIndex = NeighborGrid3D.NeighborGridIndexToLinear(AdjacentCellIndex, NeighborIdx);
+							int32 AnotherParticleIdx = NeighborGrid3D.GetParticleNeighbor(NeighborLinearIndex);
+							if (ParticleIdx == AnotherParticleIdx || AnotherParticleIdx == INDEX_NONE)
+							{
+								continue;
+							}
+
+							CalculateDensity(ParticleIdx, AnotherParticleIdx);
+						}
+					}
+
+					CalculatePressure(ParticleIdx);
+				}
+			}
+		);
+
+		// ApplyPressureが他のパーティクルの圧力値を使うので、すべて圧力値を計算してから別ループにする必要がある
+		ParallelFor(NumThreads,
+			[this, DeltaSeconds](int32 ThreadIndex)
+			{
+				for (int32 ParticleIdx = NumThreadParticles * ThreadIndex; ParticleIdx < NumThreadParticles * (ThreadIndex + 1) && ParticleIdx < NumParticles; ++ParticleIdx)
+				{
+					// キャッシュするほどのものでもないのでNeighborGrid3D構築のときと同じ計算をしているのは許容する
+					const FVector& UnitPos = NeighborGrid3D.SimulationToUnit(Positions3D[ParticleIdx], SimulationToUnitTransform);
+					const FIntVector& CellIndex = NeighborGrid3D.UnitToIndex(UnitPos);
+
+					if (!NeighborGrid3D.IsValidCellIndex(CellIndex))
+					{
+						// 構築のときに警告ログを出しているので警告を出すことはしない
+						continue;
+					}
+
+					static FIntVector AdjacentIndexOffsets[9] = {
+						FIntVector(0, -1, -1),
+						FIntVector(0, 0, -1),
+						FIntVector(0, +1, -1),
+						FIntVector(0, -1, 0),
+						FIntVector(0, 0, 0),
+						FIntVector(0, +1, 0),
+						FIntVector(0, -1, +1),
+						FIntVector(0, 0, +1),
+						FIntVector(0, +1, +1)
+					};
+
+					for (int32 AdjIdx = 0; AdjIdx < 9; ++AdjIdx)
+					{
+						const FIntVector& AdjacentCellIndex = CellIndex + AdjacentIndexOffsets[AdjIdx];
+						if (!NeighborGrid3D.IsValidCellIndex(AdjacentCellIndex))
+						{
+							continue;
+						}
+
+						for (int32 NeighborIdx = 0; NeighborIdx < MaxNeighborsPerCell; ++NeighborIdx)
+						{
+							int32 NeighborLinearIndex = NeighborGrid3D.NeighborGridIndexToLinear(AdjacentCellIndex, NeighborIdx);
+							int32 AnotherParticleIdx = NeighborGrid3D.GetParticleNeighbor(NeighborLinearIndex);
+							if (ParticleIdx == AnotherParticleIdx || AnotherParticleIdx == INDEX_NONE)
+							{
+								continue;
+							}
+
+							ApplyPressure(ParticleIdx, AnotherParticleIdx);
+							ApplyViscosity(ParticleIdx, AnotherParticleIdx);
+						}
+					}
+
+					ApplyWallPenalty(ParticleIdx);
+					Integrate(ParticleIdx, DeltaSeconds);
+				}
+			}
+		);
+	}
+	else
+	{
+		ParallelFor(NumThreads,
+			[this](int32 ThreadIndex)
+			{
+				for (int32 ParticleIdx = NumThreadParticles * ThreadIndex; ParticleIdx < NumThreadParticles * (ThreadIndex + 1) && ParticleIdx < NumParticles; ++ParticleIdx)
+				{
+					for (int32 AnotherParticleIdx = 0; AnotherParticleIdx < NumParticles; ++AnotherParticleIdx)
+					{
+						if (ParticleIdx == AnotherParticleIdx)
+						{
+							continue;
+						}
+
+						CalculateDensity(ParticleIdx, AnotherParticleIdx );
+					}
+
+					CalculatePressure(ParticleIdx);
+				}
+			}
+		);
+
+		// ApplyPressureが他のパーティクルの圧力値を使うので、すべて圧力値を計算してから別ループにする必要がある
+		ParallelFor(NumThreads,
+			[this, DeltaSeconds](int32 ThreadIndex)
+			{
+				for (int32 ParticleIdx = NumThreadParticles * ThreadIndex; ParticleIdx < NumThreadParticles * (ThreadIndex + 1) && ParticleIdx < NumParticles; ++ParticleIdx)
+				{
+					for (int32 AnotherParticleIdx = 0; AnotherParticleIdx < NumParticles; ++AnotherParticleIdx)
+					{
+						if (ParticleIdx == AnotherParticleIdx)
+						{
+							continue;
+						}
+
+						ApplyPressure(ParticleIdx, AnotherParticleIdx);
+						ApplyViscosity(ParticleIdx, AnotherParticleIdx);
+					}
+
+					ApplyWallPenalty(ParticleIdx);
+					Integrate(ParticleIdx, DeltaSeconds);
+				}
+			}
+		);
+	}
+}
+
+void ASPH3DSimulatorCPU::CalculateDensity(int32 ParticleIdx, int32 AnotherParticleIdx)
+{
+	check(ParticleIdx != AnotherParticleIdx);
+
+	const FVector2D& DiffPos = Positions[AnotherParticleIdx] - Positions[ParticleIdx];
+	float DistanceSq = DiffPos.SizeSquared();
+	if (DistanceSq < SmoothLenSq)
+	{
+		float DiffLenSq = SmoothLenSq - DistanceSq;
+		Densities[ParticleIdx] += DensityCoef * DiffLenSq * DiffLenSq * DiffLenSq;
+	}
+}
+
+void ASPH3DSimulatorCPU::CalculatePressure(int32 ParticleIdx)
+{
+	Pressures[ParticleIdx] = PressureStiffness * FMath::Max(FMath::Pow(Densities[ParticleIdx] / RestDensity, 7) - 1.0f, 0.0f);
+}
+
+void ASPH3DSimulatorCPU::ApplyPressure(int32 ParticleIdx, int32 AnotherParticleIdx)
+{
+	check(ParticleIdx != AnotherParticleIdx);
+
+	if (Densities[ParticleIdx] < SMALL_NUMBER) // 0除算と、小さな値の除算ですごく大きな項になるのを回避
+	{
+		return;
+	}
+
+	const FVector2D& DiffPos = Positions[AnotherParticleIdx] - Positions[ParticleIdx];
+	float DistanceSq = DiffPos.SizeSquared();
+	float Distance = DiffPos.Size();
+	if (DistanceSq < SmoothLenSq
+		&& Densities[AnotherParticleIdx] > SMALL_NUMBER && Distance > SMALL_NUMBER) // 0除算と、小さな値の除算ですごく大きな項になるのを回避
+	{
+		float DiffLen = SmoothLength - Distance;
+#if 1
+		// 数式と違うが、UnityGraphicsProgramming1がソースコードで使っていた式。こちらの方がなぜか安定するしフレームレートも上がる
+		float AvgPressure = 0.5f * (Pressures[ParticleIdx] + Pressures[AnotherParticleIdx]);
+		const FVector2D& Pressure = GradientPressureCoef * AvgPressure / Densities[AnotherParticleIdx] * DiffLen * DiffLen / Distance * DiffPos;
+#else
+		float DiffPressure = Pressures[ParticleIdx] - Pressures[AnotherParticleIdx];
+		const FVector2D& Pressure = GradientPressureCoef * DiffPressure / Densities[AnotherParticleIdx] * DiffLen * DiffLen / Distance * DiffPos;
+#endif
+
+		Accelerations[ParticleIdx] += Pressure / Densities[ParticleIdx];
+	}
+}
+
+void ASPH3DSimulatorCPU::ApplyViscosity(int32 ParticleIdx, int32 AnotherParticleIdx)
+{
+	check(ParticleIdx != AnotherParticleIdx);
+
+	if (Densities[ParticleIdx] < SMALL_NUMBER) // 0除算と、小さな値の除算ですごく大きな項になるのを回避
+	{
+		return;
+	}
+
+	const FVector2D& DiffPos = Positions[AnotherParticleIdx] - Positions[ParticleIdx];
+	float DistanceSq = DiffPos.SizeSquared();
+	if (DistanceSq < SmoothLenSq
+		&& Densities[AnotherParticleIdx] > SMALL_NUMBER) // 0除算と、小さな値の除算ですごく大きな項になるのを回避
+	{
+		const FVector2D& DiffVel = Velocities[AnotherParticleIdx] - Velocities[ParticleIdx];
+		const FVector2D& ViscosityForce = LaplacianViscosityCoef / Densities[AnotherParticleIdx] * (SmoothLength - DiffPos.Size()) * DiffVel;
+		Accelerations[ParticleIdx] += Viscosity * ViscosityForce / Densities[ParticleIdx];
+	}
+}
+
+void ASPH3DSimulatorCPU::ApplyWallPenalty(int32 ParticleIdx)
+{
+	//TODO: SPHって言っても加速度使わずにPBD使ってもいいはずなんだよな
+	// 上境界
+	Accelerations[ParticleIdx] += FMath::Max(0.0f, Positions[ParticleIdx].Y - WallBox.Max.Y) * WallStiffness * FVector2D(0.0f, -1.0f);
+	// 下境界
+	Accelerations[ParticleIdx] += FMath::Max(0.0f, WallBox.Min.Y - Positions[ParticleIdx].Y) * WallStiffness * FVector2D(0.0f, 1.0f);
+	// 左境界
+	Accelerations[ParticleIdx] += FMath::Max(0.0f, WallBox.Min.X - Positions[ParticleIdx].X) * WallStiffness * FVector2D(1.0f, 0.0f);
+	// 右境界
+	Accelerations[ParticleIdx] += FMath::Max(0.0f, Positions[ParticleIdx].X - WallBox.Max.X) * WallStiffness * FVector2D(-1.0f, 0.0f);
+}
+
+void ASPH3DSimulatorCPU::Integrate(int32 ParticleIdx, float DeltaSeconds)
+{
+	Accelerations[ParticleIdx] += FVector2D(0.0f, Gravity);
+	Accelerations[ParticleIdx] += FVector2D(0.0f, Gravity);
+	// 前進オイラー法
+	Velocities[ParticleIdx] += Accelerations[ParticleIdx] * DeltaSeconds;
+	Positions[ParticleIdx] += Velocities[ParticleIdx] * DeltaSeconds;
+}
+
+ASPH3DSimulatorCPU::ASPH3DSimulatorCPU()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	NiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>(TEXT("NiagaraComponent0"));
+
+	RootComponent = NiagaraComponent;
+
+#if WITH_EDITORONLY_DATA
+	SpriteComponent = CreateEditorOnlyDefaultSubobject<UBillboardComponent>(TEXT("Sprite"));
+	ArrowComponent = CreateEditorOnlyDefaultSubobject<UArrowComponent>(TEXT("ArrowComponent0"));
+
+	if (!IsRunningCommandlet())
+	{
+		// Structure to hold one-time initialization
+		struct FConstructorStatics
+		{
+			ConstructorHelpers::FObjectFinderOptional<UTexture2D> SpriteTextureObject;
+			FName ID_Effects;
+			FText NAME_Effects;
+			FConstructorStatics()
+				: SpriteTextureObject(TEXT("/Niagara/Icons/S_ParticleSystem"))
+				, ID_Effects(TEXT("Effects"))
+				, NAME_Effects(NSLOCTEXT("SpriteCategory", "Effects", "Effects"))
+			{
+			}
+		};
+		static FConstructorStatics ConstructorStatics;
+
+		if (SpriteComponent)
+		{
+			SpriteComponent->Sprite = ConstructorStatics.SpriteTextureObject.Get();
+			SpriteComponent->SetRelativeScale3D_Direct(FVector(0.5f, 0.5f, 0.5f));
+			SpriteComponent->bHiddenInGame = true;
+			SpriteComponent->bIsScreenSizeScaled = true;
+			SpriteComponent->SpriteInfo.Category = ConstructorStatics.ID_Effects;
+			SpriteComponent->SpriteInfo.DisplayName = ConstructorStatics.NAME_Effects;
+			SpriteComponent->SetupAttachment(NiagaraComponent);
+			SpriteComponent->bReceivesDecals = false;
+		}
+
+		if (ArrowComponent)
+		{
+			ArrowComponent->ArrowColor = FColor(0, 255, 128);
+
+			ArrowComponent->ArrowSize = 1.5f;
+			ArrowComponent->bTreatAsASprite = true;
+			ArrowComponent->bIsScreenSizeScaled = true;
+			ArrowComponent->SpriteInfo.Category = ConstructorStatics.ID_Effects;
+			ArrowComponent->SpriteInfo.DisplayName = ConstructorStatics.NAME_Effects;
+			ArrowComponent->SetupAttachment(NiagaraComponent);
+			ArrowComponent->SetUsingAbsoluteScale(true);
+		}
+	}
+#endif // WITH_EDITORONLY_DATA
+}
+
+void ASPH3DSimulatorCPU::PostRegisterAllComponents()
+{
+	Super::PostRegisterAllComponents();
+
+	// Set Notification Delegate
+	if (NiagaraComponent)
+	{
+		NiagaraComponent->OnSystemFinished.AddUniqueDynamic(this, &ASPH3DSimulatorCPU::OnNiagaraSystemFinished);
+	}
+}
+
+void ASPH3DSimulatorCPU::SetDestroyOnSystemFinish(bool bShouldDestroyOnSystemFinish)
+{
+	bDestroyOnSystemFinish = bShouldDestroyOnSystemFinish ? 1 : 0;  
+};
+
+void ASPH3DSimulatorCPU::OnNiagaraSystemFinished(UNiagaraComponent* FinishedComponent)
+{
+	if (bDestroyOnSystemFinish)
+	{
+		SetLifeSpan(0.0001f);
+	}
+}
+
+#if WITH_EDITOR
+bool ASPH3DSimulatorCPU::GetReferencedContentObjects(TArray<UObject*>& Objects) const
+{
+	Super::GetReferencedContentObjects(Objects);
+
+	if (UNiagaraSystem* System = NiagaraComponent->GetAsset())
+	{
+		Objects.Add(System);
+	}
+
+	return true;
+}
+
+void ASPH3DSimulatorCPU::ResetInLevel()
+{
+	if (NiagaraComponent)
+	{
+		NiagaraComponent->Activate(true);
+		NiagaraComponent->ReregisterComponent();
+	}
+}
+#endif // WITH_EDITOR
+
---
+++ Source/NiagaraSandbox/SPH/SPH3DSimulatorCPU.h
@@ -0,0 +1,150 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/ObjectMacros.h"
+#include "GameFramework/Actor.h"
+#include "../Common/NeighborGrid3DCPU.h"
+#include "SPH3DSimulatorCPU.generated.h"
+
+UCLASS()
+// ANiagaraActorを参考にしている
+class ASPH3DSimulatorCPU : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASPH3DSimulatorCPU();
+
+	virtual void PostRegisterAllComponents() override;
+
+	virtual void BeginPlay() override;
+	virtual void Tick( float DeltaSeconds ) override;
+
+	/** Set true for this actor to self-destruct when the Niagara system finishes, false otherwise */
+	UFUNCTION(BlueprintCallable)
+	void SetDestroyOnSystemFinish(bool bShouldDestroyOnSystemFinish);
+
+private:
+	/** Pointer to System component */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UNiagaraComponent* NiagaraComponent;
+
+#if WITH_EDITORONLY_DATA
+	// Reference to sprite visualization component
+	UPROPERTY()
+	class UBillboardComponent* SpriteComponent;
+
+	// Reference to arrow visualization component
+	UPROPERTY()
+	class UArrowComponent* ArrowComponent;
+
+#endif
+
+	/** True for this actor to self-destruct when the Niagara system finishes, false otherwise */
+	UPROPERTY()
+	uint32 bDestroyOnSystemFinish : 1;
+
+	/** Callback when Niagara system finishes. */
+	UFUNCTION(CallInEditor)
+	void OnNiagaraSystemFinished(UNiagaraComponent* FinishedComponent);
+
+	UPROPERTY(EditAnywhere)
+	int32 NumThreads = 4;
+
+	UPROPERTY(EditAnywhere)
+	bool bUseNeighborGrid3D = true;
+
+	UPROPERTY(EditAnywhere)
+	int32 NumParticles = 1000;
+
+	UPROPERTY(EditAnywhere)
+	int32 NumIterations = 4;
+
+	UPROPERTY(EditAnywhere)
+	float FrameRate = 60.0f;
+
+	UPROPERTY(EditAnywhere)
+	FBox2D WallBox = FBox2D(FVector2D(-4.5f, -4.5f), FVector2D(4.5f, 4.5f));
+
+	UPROPERTY(EditAnywhere)
+	float WallStiffness = 3000.0f;
+
+	UPROPERTY(EditAnywhere)
+	float Gravity = -9.81f;
+
+	UPROPERTY(EditAnywhere)
+	float Mass = 0.08f;
+
+	UPROPERTY(EditAnywhere)
+	float SmoothLength = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float RestDensity = 4.0f;
+
+	UPROPERTY(EditAnywhere)
+	float PressureStiffness = 0.57f;
+
+	UPROPERTY(EditAnywhere)
+	float Viscosity = 3.0f;
+
+	UPROPERTY(EditAnywhere)
+	float InitPosRadius = 4.0f;
+
+	UPROPERTY(EditAnywhere)
+	int32 NumCellsX = 10;
+
+	UPROPERTY(EditAnywhere)
+	int32 NumCellsY = 10;
+
+	UPROPERTY(EditAnywhere)
+	int32 MaxNeighborsPerCell = 8;
+
+	UPROPERTY(EditAnywhere)
+	FVector2D WorldBBoxSize = FVector2D(10.0f, 10.0f);
+
+private:
+	void Simulate(float DeltaSeconds);
+	void CalculateDensity(int32 ParticleIdx, int32 AnotherParticleIdx);
+	void CalculatePressure(int32 ParticleIdx);
+	void ApplyPressure(int32 ParticleIdx, int32 AnotherParticleIdx);
+	void ApplyViscosity(int32 ParticleIdx, int32 AnotherParticleIdx);
+	void ApplyWallPenalty(int32 ParticleIdx);
+	void Integrate(int32 ParticleIdx, float DeltaSeconds);
+
+private:
+	TArray<FVector2D> Positions;
+	TArray<FVector2D> Velocities;
+	// 加速度は毎フレーム計算するのでフレーム間のひきつぎはないのだが、使用メモリやTArrayの生成負荷をおさえるために
+	// 使いまわしている
+	TArray<FVector2D> Accelerations;
+	TArray<float> Densities;
+	TArray<float> Pressures;
+	TArray<FVector> Positions3D;
+	float DensityCoef = 0.0f;
+	float GradientPressureCoef = 0.0f;
+	float LaplacianViscosityCoef = 0.0f;
+	float SmoothLenSq = 0.0f;
+	int32 NumThreadParticles = 0.0f;
+	FNeighborGrid3DCPU NeighborGrid3D;
+	FTransform SimulationToUnitTransform;
+
+public:
+	/** Returns NiagaraComponent subobject **/
+	class UNiagaraComponent* GetNiagaraComponent() const { return NiagaraComponent; }
+#if WITH_EDITORONLY_DATA
+	/** Returns SpriteComponent subobject **/
+	class UBillboardComponent* GetSpriteComponent() const { return SpriteComponent; }
+	/** Returns ArrowComponent subobject **/
+	class UArrowComponent* GetArrowComponent() const { return ArrowComponent; }
+#endif
+
+#if WITH_EDITOR
+	// AActor interface
+	virtual bool GetReferencedContentObjects(TArray<UObject*>& Objects) const override;
+	// End of AActor interface
+
+	/** Reset this actor in the level.*/
+	void ResetInLevel();
+#endif // WITH_EDITOR
+};
+
